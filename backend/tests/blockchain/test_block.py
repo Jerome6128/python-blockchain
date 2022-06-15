daa9ba466bb5be48c0c16f16a99a8daec4ff2cc3
Jerome Gratelle <jerome.gratelle@air-indemnite.com>
2022-06-15 16:32:14 +0200

    validate and replace a blockchain

--- backend/tests/blockchain/test_block.py
+++ backend/tests/blockchain/test_block.py
@@ -1,4 +1,6 @@
+import pytest
 import time
+
 from backend.blockchain.block import Block, GENESIS_DATA
 from backend.config import MINE_RATE, SECONDS
 from backend.util.hex_to_binary import hex_to_binary
@@ -51,3 +53,43 @@ def test_mined_block_difficulty_limits_at_1():
     mined_block = Block.mine_block(last_block, 'bar')
     
     assert mined_block.difficulty == last_block.difficulty
+
+
+@pytest.fixture
+def last_block():
+    return Block.genesis()
+
+@pytest.fixture
+def block(last_block):
+    return Block.mine_block(last_block, 'test_data')
+
+def test_is_valid_block(last_block, block):
+    Block.is_valid_block(last_block, block)
+    
+    
+def test_is_valid_block_bad_last_hash(last_block, block):
+    block.last_hash = 'evil_last_hash'
+    
+    with pytest.raises(Exception, match="The block last_hash must be correct"):
+       Block.is_valid_block(last_block, block)
+
+
+def test_is_valid_block_bad_proof_of_work(last_block, block):
+    block.hash = 'evil_hash'
+    
+    with pytest.raises(Exception, match="The POW requirement was not met"):
+       Block.is_valid_block(last_block, block)
+       
+       
+def test_is_valid_block_jumped_difficulty(last_block, block):
+    block.difficulty = last_block.difficulty + 2
+    block.hash = f"{'0' * block.difficulty}111abc"
+    
+    with pytest.raises(Exception, match="The difficulty must only adjust by 1"):
+       Block.is_valid_block(last_block, block)
+       
+def test_is_valid_block_bad_block_hash(last_block, block):
+    block.hash =  f"{'0' * block.difficulty}111abc"
+
+    with pytest.raises(Exception, match="The block hash must be a valid combination of the block fields"):
+       Block.is_valid_block(last_block, block)
--- backend/tests/blockchain/test_blockchain.py
+++ backend/tests/blockchain/test_blockchain.py
@@ -1,6 +1,8 @@
-from codecs import backslashreplace_errors
+import pytest
+
 from backend.blockchain.blockchain import Blockchain
 from backend.blockchain.block import GENESIS_DATA
+from backend.tests.blockchain.test_block import block
 
 def test_block_chain_instance():
     blockchain = Blockchain()
@@ -13,4 +15,48 @@ def test_add_block():
     blockchain.add_block(data)
     
     assert blockchain.chain[-1].data == data
+
+
+@pytest.fixture
+def blockchain_three_blocks():
+    blockchain = Blockchain()
     
+    for i in range(3):
+        blockchain.add_block(i)  
+        
+    return blockchain  
+
+def test_is_valid_chain(blockchain_three_blocks):
+    Blockchain.is_valid_chain(blockchain_three_blocks.chain)
+    
+
+def test_is_valid_chain_bad_genesis(blockchain_three_blocks):
+    
+    blockchain_three_blocks.chain[0].data = "evil_genesis"
+        
+    with pytest.raises(Exception, match="The genesis block must be valid"):
+       Blockchain.is_valid_chain(blockchain_three_blocks.chain)
+
+    
+def test_replace_chain(blockchain_three_blocks):
+    blockchain = Blockchain()
+    blockchain.replace_chain(blockchain_three_blocks.chain)
+    
+    assert blockchain.chain == blockchain_three_blocks.chain
+
+
+def test_replace_chain_not_longer(blockchain_three_blocks):
+    blockchain = Blockchain()
+    
+    with pytest.raises(Exception, match="Cannot replace. The incoming chain must be longer"):
+        blockchain_three_blocks.replace_chain(blockchain.chain)
+ 
+        
+def test_replace_chain_not_valid(blockchain_three_blocks):
+    blockchain = Blockchain()
+    blockchain_three_blocks.chain[-1].data = "evil_data"
+    
+    with pytest.raises(Exception, match="Cannot replace. The incoming chain is invalid:"):
+        blockchain.replace_chain(blockchain_three_blocks.chain)
+    
+
